b0987b22b23e4e68c64e82dc0cee160a5395edcf
François Degryse <fdegryse@ankama.com>
2017-12-14 16:31:39 +0100

    Basic controls and physics

---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using JetBrains.Annotations;
+using Rewired;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[UsedImplicitly]
+	public int playerId;
+
+	[UsedImplicitly]
+	public Vector2 moveFactor;
+
+	[UsedImplicitly]
+	public float torqueFactor;
+
+	[UsedImplicitly]
+	public Rigidbody2D mainRigidbody;
+
+	private Player m_player;
+	private bool m_initialized;
+
+	private void Initialize() 
+	{
+		m_player = ReInput.players.GetPlayer(playerId);
+		m_initialized = true;
+	}
+	
+	[UsedImplicitly]
+	private void Update () 
+	{
+		if (!ReInput.isReady)
+		{
+			return;
+		}
+		if (!m_initialized)
+		{
+			Initialize();
+		}
+
+		float x = m_player.GetAxis("MoveHorizontal");
+		float y = m_player.GetAxis("MoveVertical");
+
+		x *= moveFactor.x;
+		y *= moveFactor.y;
+
+		Vector2 moveVector = new Vector2(x, y);
+
+		if (moveVector.sqrMagnitude > float.Epsilon)
+		{
+			Vector2 force = moveVector * Time.deltaTime;
+			mainRigidbody.AddForce(force, ForceMode2D.Force);
+		}
+
+		float angle = Vector3.SignedAngle(Vector3.up, mainRigidbody.transform.up, Vector3.forward);
+		if (Mathf.Abs(angle) > float.Epsilon)
+		{
+			float torque = -angle * torqueFactor * Time.deltaTime;
+			mainRigidbody.AddTorque(torque, ForceMode2D.Force);
+		}
+	}
+}
